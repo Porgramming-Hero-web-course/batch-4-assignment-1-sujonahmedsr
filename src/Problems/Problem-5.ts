31543b5d08c848ea322773cdc6aa63922f7a0474
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-11-08 20:16:51 +0600

    Fifth problem solved

--- src/Problems/Problem-5.ts
+++ src/Problems/Problem-5.ts
@@ -1,3 +1,7 @@
-// function getProperty <T, X keyof T> (T, X){
+function getProperty <T, X extends keyof T> (value: T, property: X){
+    return value[property]
+}
 
-// }
+const person = { name: "Alice", age: 30, email: 'shofiqul islam sujon' };
+
+// console.log(getProperty(person, 'name'));
