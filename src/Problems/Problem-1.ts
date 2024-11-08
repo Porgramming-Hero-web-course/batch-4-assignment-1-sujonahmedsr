c30d86ab24dfc3c18844a74b8f0d852e3252380f
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-11-08 19:03:17 +0600

    first problem solved

---
+++ src/Problems/Problem-1.ts
@@ -0,0 +1,4 @@
+function sumArray(vlaues: number[]): number{
+    return vlaues.reduce((total, value) => total + value, 0)
+}
+// console.log(sumArray([1, 2, 3, 4, 5, 5]));
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log('hellow testing');
+let myName: string = 'sujon ahmed'
+console.log(myName);
+
+let roll: number = 52
+console.log(roll);
+
+
