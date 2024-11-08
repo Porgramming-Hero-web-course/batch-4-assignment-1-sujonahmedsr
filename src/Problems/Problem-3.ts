465ade9a8464cc6ab84d71bb69451ff791dfc867
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-11-08 19:19:03 +0600

    Third problem solved

---
+++ src/Problems/Problem-3.ts
@@ -0,0 +1,11 @@
+function countWordOccurrences(sentence: string, word: string) : number {
+    let makeArray = sentence.toLowerCase().split(' ')
+    let number = 0
+    for (const element of makeArray) {
+        if (element === word.toLowerCase()) {
+            number += 1
+        }
+    }
+    return number
+}
+console.log(countWordOccurrences("I love typescript", "Typescript"));
