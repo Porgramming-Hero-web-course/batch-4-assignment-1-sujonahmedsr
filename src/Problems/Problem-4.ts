8af2e20072ed2ce4c56e64e302ed78ca071f09fc
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-11-08 19:33:00 +0600

    Fourth problem solved

---
+++ src/Problems/Problem-4.ts
@@ -0,0 +1,34 @@
+type Circle = {
+    shape: 'circle',
+    radius: number
+}
+type Rectangle = {
+    shape: 'rectangle',
+    width: number,
+    height: number
+}
+
+type Area = Circle | Rectangle;
+
+function calculateShapeArea(area: Area) {
+    if (area.shape === 'circle') {
+        let areaOfCircle = (Math.PI * (area.radius * area.radius)).toFixed(2)
+        return areaOfCircle
+    } else if (area.shape === 'rectangle') {
+        return area.width * area.height
+    }else{
+        return `Please provide correct information`
+    }
+}
+
+// const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+// console.log(circleArea);
+
+// const rectangleArea = calculateShapeArea({
+//     shape: "rectangle",
+//     width: 4,
+//     height: 6,
+// });
+// console.log(rectangleArea);
+
+
