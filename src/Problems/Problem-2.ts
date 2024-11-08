c96e1a8a6077d8816b47eb8eb7025699d9e87fc6
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-11-08 19:03:44 +0600

    Second  problem solved

---
+++ src/Problems/Problem-2.ts
@@ -0,0 +1,4 @@
+function removeDuplicates(arr: number[]) : number[]{
+    return arr.filter((value, index) => arr.indexOf(value) === index).sort((a, b) => a - b)
+}
+// console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]));
